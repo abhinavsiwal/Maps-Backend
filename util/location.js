9c81e9b26eef87d1d09e6464a22ee071cba9ee64
abhinavsiwal <abhinavsiwal@gmail.com>
2021-10-13 15:42:36 +0530

    Fix

--- util/location.js
+++ util/location.js
@@ -4,22 +4,23 @@ const HttpError=require('../models/http-error')
 const API_KEY=process.env.GOOGLE_API_KEY;
 
 const getCoordsForAddress=async(address)=>{
-    // const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
 
-    // const data = response.data;
-    // console.log(address);
-    // console.log(data);
-    // if(!data||data.status==='ZERO_RESULTS'){
-    //     const error = new HttpError('Could not find location for specified Address',422);
-    //     throw error;
-    // }
-    // const coordinates=data.results[0].geometry.location;
-    // return coordinates;
-    const coordinates={
-        lat:40.7484474,
-        lng:-73.9871516,
+    const data = response.data;
+    console.log(address);
+    console.log(data);
+    if(!data||data.status==='ZERO_RESULTS'){
+        const error = new HttpError('Could not find location for specified Address',422);
+        throw error;
     }
+    const coordinates=data.results[0].geometry.location;
     return coordinates;
+
+//     const coordinates={
+//         lat:40.7484474,
+//         lng:-73.9871516,
+//     }
+//     return coordinates;
 }
 
 module.exports=getCoordsForAddress;
